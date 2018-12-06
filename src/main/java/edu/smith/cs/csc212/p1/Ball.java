395aa9712a8b2fd7e99a144a828ff2795cb0d389
Jo7Edwards <candypop777@gmail.com>
2018-12-06 14:57:15 -0500

    friggin ball, at least it moves

--- src/main/java/edu/smith/cs/csc212/p1/Ball.java
+++ src/main/java/edu/smith/cs/csc212/p1/Ball.java
@@ -6,25 +6,41 @@ import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.util.Random;
 
+import me.jjfoley.gfx.GFX;
+
 public class Ball {
 	
 	int x;
-	int y;
+	double y;
 	Color color;
 	boolean travelR = true;
 	boolean travelUp = true;
 	
 	int size = 12;
 	
+	double speedx;
+	double speedy;
+	int speed;
+	
 	
 	Random rand = new Random();
-	int DestX = rand.nextInt(500);
+	int DestX = 500;
 	int DestY = rand.nextInt(500);
+	double newthing = rand.nextDouble();
 	
 	public Ball(Color color, int x, int y) {
 		this.color = color;
 		this.y = y;
 		this.x = x; 
+		
+		this.speed = 1;
+		
+		this.speedx = 2;
+		this.speedy = 0.8;
+		
+		//int subY = this.y;
+		//int distY = DestY - subY;
+		//this.subY = distY;
 		
 	}
 	
@@ -41,7 +57,10 @@ public class Ball {
 	
 	public void MoveBall(Graphics2D g) {
 		int speed = 1;
-		/*x needs to always be moving +1 or -1, but Y can have random placement */
+		
+		
+		
+		/*x needs to always be moving +1 or -1, but Y can have random placement 
 		//int distX = this.DestX - this.x;
 		
 		int distY = this.DestY - this.y;//upward downward or even 
@@ -71,13 +90,69 @@ public class Ball {
 		} else if (travelUp == false) {
 			this.y -=speed;
 		}
-		//If randNum equals 2, then choose whether to shoot towards CPU or player
+		//If randNum equals 2, then choose whether to shoot towards CPU or player*/
+		
+		//double distY = DestY / 200;
+		
+		//this.speedy = distY;
+		//double moveY = this.subY/100; 
+		//speedy = moveY;
+		
+		//this.speedy = 0.8;
+		//------------------------------------------------------------------------------------
+		//this.x += this.speedx;
+		//this.y += this.speedy;
+		
+		
+		this.x += this.speedx;
+		this.y += this.speedy;
 		
 		
 		
+		//check hitting top or bottom wall
+		if (this.y <= 0 || this.y >= 500) {
+			
+			speedy = 0 - speedy; //change to opposite direction if hit top
+		} 
+		
+		//check if hitting left or right wall 
+		if (this.x <=0 || this.x >= 500) {
+			//this.DestY = rand.nextInt(500);
+			
+			speedx = 0 - speedx;
+		}
 		
 		
 		
+	}
+	
+	/**
+	 * changes the destination point 
+	 */
+	public void changeDest() {
 		
+		if (travelR == true) {
+			this.DestX = 500;
+		} else {
+			this.DestX = 0;
+		}
+		
+		this.DestY = rand.nextInt(500);
+	}
+	
+	/**
+	 * Figures out the angle and changes this.speedx and speedy accordingly 
+	 */
+	public void findAngle() {
+		float difX = DestX - this.x;
+		float difY = (float) (DestY - this.y);
+		
+		double theta = Math.atan2(difY, difX);
+		
+		double movex = speed * Math.cos(theta);
+		double movey = speed * Math.sin(theta);
+		
+		this.speedx = movex;
+		this.speedy = movey;
 	}
 }
